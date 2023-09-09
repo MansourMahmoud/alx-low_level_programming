40f408dacd0119a574c7cceeff0fd72658f09447
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-09 16:11:00 -0700

    add file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *	main - this is a function for practicing
+ *	Return: 0 always
+ */
+
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a' ; letter <= 'z' ; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
