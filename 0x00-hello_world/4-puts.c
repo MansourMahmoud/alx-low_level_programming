3bb791cba83f81e8bc6a42c7fac738600ee8349e
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-08 22:36:52 -0700

    add files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this function for practcing
+ * Return: 0 always
+ */
+
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
