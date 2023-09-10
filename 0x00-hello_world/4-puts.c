a045e2426ba238f54b939d0209d6b1deb32ba459
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-09 19:21:50 -0700

    edit file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,8 @@
  * main - this function for practcing
  * Return: 0 always
  */
-
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n\n\n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }
