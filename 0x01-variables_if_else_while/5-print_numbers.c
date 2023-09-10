821eec85cf49b846c757fada69ea16ddabdbc767
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-09 17:31:44 -0700

    add file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *	main - this is a function for practicing
+ *	Return: 0 always
+ */
+
+int main(void)
+{
+
+	int singleDigit = 0;
+
+	while (singleDigit < 10)
+	{
+		printf("%d", singleDigit);
+		singleDigit++;
+	}
+	printf("\n");
+	return (0);
+}
