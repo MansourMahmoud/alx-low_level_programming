cf87c0c2ad7a0f1ca71450e383105922d0869bba
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-13 10:51:23 -0700

    add file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *	print_alphabet_x10 - this is a function for practicing
+ *	Return: should print 0 always
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i <= 9; i--)
+	{
+	for (int c = "a"; c <= "z"; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	}
+}
