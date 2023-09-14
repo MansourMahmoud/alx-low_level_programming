205ef294aa2895163f2eb4d69bfa3866a1ae71c9
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-13 19:42:50 -0700

    add file

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *	_abs - this is a function for pacticing
+ *	@n: check input of function
+ *	Return: always 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+
+
+
+}
