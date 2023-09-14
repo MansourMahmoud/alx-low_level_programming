71cfd57573b5892da46999c383469c177c8f5e22
MansourMahmoud <mansourmahmoud77a@gmail.com>
2023-09-13 19:32:20 -0700

    add files

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *	_isalpha - this is a function for pacticing
+ *	@c: check input of function
+ *	Return: 1 if 'c' is lowercase
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+
+}
